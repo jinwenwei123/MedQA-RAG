b41ef0c10426a5a650a4f9d21f8971347affb450
AshHikari <jinwenwei123@nuaa.edu.cn>
2025-12-24 20:31:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 config = {
-    "question_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\question.csv',
-    "answer_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\answer.csv',
-    "train_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\train_candidates.txt',
-    "dev_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\dev_candidates.txt',
-    "test_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\test_candidates.txt'
+    "question_dict": r'E:\PyProjects\MedQA-RAG\dataset\cMedQA2\question.csv',
+
+    "answer_dict": r'E:\PyProjects\MedQA-RAG\dataset\cMedQA2\answer.csv',
+
+    "train_dict": r'E:\PyProjects\MedQA-RAG\dataset\cMedQA2\train_candidates.txt',
+
+    "dev_dict": r'E:\PyProjects\MedQA-RAG\dataset\cMedQA2\dev_candidates.txt',
+
+    "test_dict": r'E:\PyProjects\MedQA-RAG\dataset\cMedQA2\test_candidates.txt',
+
+    "samples_num": 100,
+    "citation_fallback_mode": "topk"
 }
