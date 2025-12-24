92a9b6326513fae86bb87ae874140c43d44ae9e0
AshHikari <jinwenwei123@nuaa.edu.cn>
2025-12-24 09:46:58 +0800

    Add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    "question_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\question.csv',
+    "answer_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\answer.csv',
+    "train_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\train_candidates.txt',
+    "dev_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\dev_candidates.txt',
+    "test_dict": r'F:\pythonProject\MedQA-RAG\dataset\cMedQA2\test_candidates.txt'
+}
